771795256da541a37a5a1e3b8700a0d3c9d63027
ice <createfriceice@gmail.com>
2021-04-22 22:20:01 +0800

    项目的创建

---
+++ src/test/java/com/example/face_recognition/FaceRecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.face_recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceRecognitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
